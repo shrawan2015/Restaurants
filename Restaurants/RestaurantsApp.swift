aa7a2d22c2a1871e658717a65c9f9363cba9586a
Shrawan Kr Sharma <shrawan25.sharma@gmail.com>
2023-09-06 20:20:45 +0530

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Shrawan Kumar sharma on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
